0edc2d2dd6be062295506fa08b1bffbbf70d5e7f
Kamil Pawlak <milpawlak16@gmail.com>
2025-07-05 19:28:39 +0200

    Added use of api endpoint by using swr in channelspanel (#7)

--- electron-app/src/components/ChannelsPanel.tsx
+++ electron-app/src/components/ChannelsPanel.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import ChannelButton  from './ChannelButton';
+import request from '../utils/request';
+import {Channel} from '../types/channel';
+import useSWR from 'swr';
 
 type Props = {
-  activeChannel: { id: string; name: string } | null;
-  onChannelSelect?: (channel: { id: string; name: string }) => void;
+  activeChannel: Channel | null;
+  onChannelSelect?: (channel: Channel) => void;
 }
+
 
 const ChannelsPanel = (props: Props) => {
 
+  const{
+    data: channels,
+    error,
+    isValidating,
+  } = useSWR<Array<Channel>>('http://localhost:5000/channels', request);
+
+   if (error) return <div className="w-64 bg-gray-800 h-full border-gray-500 border-1 text-white p-2 flex flex-col gap-0.5">Failed to load</div>;
+  if (isValidating) return <div className="w-64 bg-gray-800 h-full border-gray-500 border-1 text-white p-2 flex flex-col gap-0.5">Loading</div>;
   
-  const channels = [
-    { id: "1", name: "general" },
-    { id: "2", name: "music" },
-    { id: "3", name: "memes" }
-  ];
+
   return (
     <div className="w-64 bg-gray-800 h-full border-gray-500 border-1 text-white p-2 flex flex-col gap-0.5">
-      {channels.map((channel) => (
+      {channels?.map((channel: Channel) => (
         
         <ChannelButton key={channel.id} channel={{...channel}} onChannelSelect={props.onChannelSelect} 
-        isActive={props.activeChannel?.id == channel.id}/>
+        isActive={props.activeChannel?.id === channel.id}/>
       ))}
     </div>
   )
