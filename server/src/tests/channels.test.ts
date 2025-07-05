54eced97d2917a4dbb87ffbe0f7bf87286e33873
Kamil Pawlak <milpawlak16@gmail.com>
2025-07-05 17:34:12 +0200

    Initializing api and tests. (#6)
    * Added api and one router with endpoint get: /channels that returns list of channels
    
    * added test for get /channels endpoint and broke index.ts to app.ts and index.ts
    
    * fixed folder tree structure (moved tests to /src/tests)

---
+++ server/src/tests/channels.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import app from '../app';
+
+describe("GET /channels", () =>{
+    it("should return list of available channels", async () =>{
+        const res = await request(app)
+        .get("/channels")
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+        expect(Array.isArray(res.body)).toBe(true);
+        res.body.forEach((item: { id:string; name: string }) => {
+            expect(typeof item.id).toBe("string");
+            expect(typeof item.name).toBe("string");
+        });
+    });
+});
