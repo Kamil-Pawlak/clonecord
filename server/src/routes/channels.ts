69b7fdc92127f3ea301fa4dd917780e8d62f11d0
Kamil Pawlak <milpawlak16@gmail.com>
2025-07-06 20:32:37 +0200

    Added post /channel endpoint that creates new channel and created tests for it (#8)

--- server/src/routes/channels.ts
+++ server/src/routes/channels.ts
@@ -1,13 +1,33 @@
 import express from 'express'
+import {Channel} from '../types/channel';
+import { randomUUID } from 'crypto';
 
 const router = express.Router();
 
+let channels: Array<Channel> = [];
+
 router.get('/', (req,res) =>{
     res.json(
-        [{id: "1", name:'general'},
-        {id: "2", name:'music'},
-        {id: "3", name:'memes'}]
+        channels
     );
+});
+
+router.post('/', (req, res) => {
+    if(!req.body)
+    {
+        res.status(400).json();
+        return;
+    }
+    const {name} = req.body;
+    if(typeof name != 'string' || !name || name.trim() === "")
+    {
+       res.status(400).json({error: "Invalid channel name"});
+    }
+    else{
+        channels.push({id: `${randomUUID()}`, name: req.body.name});
+        res.status(201).json();
+    }
+   
 });
 
 export default router;
--- server/src/tests/channels.test.ts
+++ server/src/tests/channels.test.ts
@@ -15,3 +15,52 @@ describe("GET /channels", () =>{
         });
     });
 });
+
+describe("POST /channels", () =>{
+    it("should create a channel when provided a name", async () =>{
+        //test good input
+        await request(app)
+        .post("/channels")
+        .send({name: "test channel"})
+        .expect("Content-Type", /json/)
+        .expect(201);
+    })
+});
+
+describe("POST /channels", () =>{
+    it("testing empty body", async () =>{
+        //test empty input
+        await request(app)
+        .post("/channels")
+        .send()
+        .expect("Content-Type", /json/)
+        .expect(400);
+    })
+});
+
+
+describe("POST /channels", () =>{
+    it("testing empty name field", async () =>{
+        //test empty name field
+        await request(app)
+        .post("/channels")
+        .send({name: ""})
+        .expect("Content-Type", /json/)
+        .expect(400);
+    })
+});
+
+
+describe("POST /channels", () =>{
+    it("testing wrong data type", async () =>{
+        //test wrong data type
+        await request(app)
+        .post("/channels")
+        .send({name: 1})
+        .expect("Content-Type", /json/)
+        .expect(400);
+    })
+});
+
+
+
